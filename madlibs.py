a44d08d0b5bfb716e0a9be0699c6e4f215dd9e73
mavoo-a11y <marvinlone456@gmail.com>
2025-03-28 12:25:21 +0300

    madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+adjective1 = input("Enter an adjective(description):")
+noun1 = input("enter a noun(person,place,thing):")
+adjective2 = input("enter an adjetive(description):")
+verb1 = input("enter a verb ending with 'ing'")
+adjective3= input("enter an adjetive(description):")
+
+
+print(f"Today i met{adjective1}Elon Musk.")
+print(f"In an exhibit,i saw a {noun1}")
+print(f"{noun1}was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")

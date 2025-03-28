ce96194d75f382d94ce4388bee79de7fc890214b
mavoo-a11y <marvinlone456@gmail.com>
2025-03-28 12:24:42 +0300

    import_math.py

---
+++ import_math.py
@@ -0,0 +1,37 @@
+#import math
+
+#x = 9.9
+
+#print(math.pi)
+#print(math.e)
+#result = math.sqrt(x)
+#result = math.ceil(x)
+
+#result = math.floor(x)
+
+#print(result)
+
+#import math
+
+#radius = float(input('Enter the radius of the circle:'))
+
+#circumference = 2 * math.pi * radius
+
+#print(f"The circumference is: {round(circumference, 2)}cm")
+
+#import math 
+
+#radius = float(input("Enter the radius of the circle:"))
+
+#area = math .pi* pow(radius,2)
+
+#print(f"The area of the circle is: {round(area, 2)}cm^2")
+
+import math 
+
+a = float(input('Enter side A: '))
+b = float(input('Enter side B: '))
+
+C = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"side C = {C}")

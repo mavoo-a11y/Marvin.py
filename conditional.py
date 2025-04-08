74a492d337ae051fddbd5307251e14ce1e3b864f
mavoo-a11y <marvinlone456@gmail.com>
2025-04-08 19:54:39 +0300

    conditional.py

---
+++ conditional.py
@@ -0,0 +1,15 @@
+num = -5
+
+# print("Positive"if num > 0 else "Negative")
+
+num = 6
+a = 6
+b = 7
+age = 15
+
+# result = "Even" if num% 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+status = "Adult"if age >= 18 else "Child"
+
+print(status)

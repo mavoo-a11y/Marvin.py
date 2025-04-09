aa208e52dcd2bfffcb52f6e668a9ef3d61710c49
mavoo-a11y <marvinlone456@gmail.com>
2025-04-09 19:42:31 +0300

    indexing.py

---
+++ indexing.py
@@ -0,0 +1,11 @@
+credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[4])
+# print(credit_number[0:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-3])
+# print(credit_number[::2])
+
+last_digits = credit_number[-4:]
+print(f'XXXX-XXXX-XXXX-{last_digits}')

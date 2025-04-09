5b747297ba5404d04e46d3f64ebc98a13c30732b
mavoo-a11y <marvinlone456@gmail.com>
2025-04-09 19:41:33 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+name = input("Enter your full name: ")
+
+# result = len(name)
+# result = name.find("a")
+# result = name.rfind("a")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+result = name.isdigit()
+
+print(result)
